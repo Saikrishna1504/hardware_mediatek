895a1be968fd4ca3e611e902b0c932c5f15d4c59
claxten10 <claxten10@gmail.com>
2024-12-14 14:22:23 +0000

    aee: Provide a dummy `libaedv`
    * On AOSP, AEE is completely useless, but a few blobs depend on libaedv.
    * Provide a stub library that will provide only the required symbols to
      ensure the blobs that depend on libaedv work without needing the whole
      proprietary AEE lib.
    
    test:
     - audio
     - camera
     - ril
     - other that depend on libaedv
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

---
+++ libaedv/aee.c
@@ -0,0 +1,28 @@
+//
+// SPDX-FileCopyrightText: The LineageOS Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#define LOG_TAG "libaedv"
+
+#include <log/log.h>
+
+int aee_switch_ftrace(int status) {
+    ALOGD("[%s]: status: %d", __func__, status);
+    return 0;
+}
+
+int aee_system_exception(const char *lib, const char *path, unsigned int options, const char *log_msg) {
+    ALOGD("[%s]: lib: %s, path: %s, options: %u, log_msg: %s", __func__, lib, path, options, log_msg);
+    return 0;
+}
+
+int aee_system_warning(const char *lib, const char *path, unsigned int options, const char *log_msg) {
+    ALOGD("[%s]: lib: %s, path: %s, options: %u, log_msg: %s", __func__, lib, path, options, log_msg);
+    return 0;
+}
+
+int aee_log_msg(const char *lib, const char *path, unsigned int options, const char *log_msg, const char *ver) {
+    ALOGD("[%s]: lib: %s, path: %s, options: %u, log_msg: %s, ver: %s", __func__, lib, path, options, log_msg, ver);
+    return 0;
+}
